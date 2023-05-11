b4b55e6016a897c908598c743c0fcc954750704b
“mscanzillo” <scanzillom@gmail.com>
2023-05-10 20:48:41 -0400

    traffic light exercise

--- Week 4 - Intro to JS/Lesson 08/05_traffic_light/mes_solution/script.js
+++ Week 4 - Intro to JS/Lesson 08/05_traffic_light/mes_solution/script.js
@@ -1,2 +1,43 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
+
+// Function to turn all lights off
+function turnOffLights() {
+    // Turn off all the lights
+    document.getElementById("stopLight").classList.remove("glow");
+    document.getElementById("slowLight").classList.remove("glow");
+    document.getElementById("goLight").classList.remove("glow");
+}
+
+// Function to turn stop on
+function stopLight() {
+   // turn off lights
+    turnOffLights
+    // Turn on one of the lights
+    document.getElementById("stopLight").classList.toggle("glow");
+}
+
+// Function to turn slow on
+function slowLight() {
+    // turn off lights
+     turnOffLights()
+     // Turn on one of the lights
+     document.getElementById("slowLight").classList.toggle("glow");
+ }
+
+// Function to turn go on
+ function goLight() {
+    // turn off lights
+     turnOffLights()
+     // Turn on one of the lights
+     document.getElementById("goLight").classList.toggle("glow");
+ }
+
+
+let stopButton = document.getElementById("stopButton");
+let slowButton = document.getElementById("slowButton");
+let goButton = document.getElementById("goButton");
+
+stopButton.addEventListener('click', stopLight);
+slowButton.addEventListener('click', slowLight);
+goButton.addEventListener('click', goLight);
