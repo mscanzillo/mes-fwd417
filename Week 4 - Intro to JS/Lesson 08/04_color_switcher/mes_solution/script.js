be0718094892b4e186c291690153d4fb1f9dc04b
“mscanzillo” <scanzillom@gmail.com>
2023-05-10 20:24:57 -0400

    adding exercise 5 and renaming exercise 4

--- Week 4 - Intro to JS/Lesson 08/04_color_switcher/mes_solution/script.js
+++ Week 4 - Intro to JS/Lesson 08/04_color_switcher/mes_solution/script.js
@@ -2,3 +2,33 @@
 // The #whiteButton should bring things back to the "light mode" color scheme with white backgrounds and dark text
 // Try to do this by setting styles (.style)
 // Remember to try one step at a time, testing each stage as you go!
+
+// DEFINE:
+//  a function that turns on the gray scheme
+    function setGrayScheme(){
+        let body = document.querySelector("body");
+        //set the body background
+        body.style.backgroundColor = 'gray';
+        //set the text color 
+        body.style.color = 'white';
+    }
+    
+//  a function that turns on the white scheme
+    function setWhiteScheme(){
+        let body = document.querySelector("body");
+        //set the body background
+        body.style.backgroundColor = 'white';
+        //set the text color 
+        body.style.color = 'black';
+    }
+
+// OBSERVE:
+// create objects for two buttons
+let grayButton = document.getElementById("grayButton");
+let whiteButton = document.getElementById("whiteButton");
+
+// INVOKE:
+// trigger functions when you click on grey
+// trigger functions when you click on white 
+grayButton.addEventListener('click', setGrayScheme);
+whiteButton.addEventListener('click', setWhiteScheme);
