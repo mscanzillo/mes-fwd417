9c5de2d5165b2c6cc45cae6b5484d37d692c2eb8
“mscanzillo” <scanzillom@gmail.com>
2023-05-15 20:38:45 -0400

    changes to week 5 - L9

--- Week 5 - More JS & Responsive Design/Lesson 09/01_moods/mes_solution/js/app.js
+++ Week 5 - More JS & Responsive Design/Lesson 09/01_moods/mes_solution/js/app.js
@@ -1,19 +1,32 @@
 //wait for the DOM elements to load before executing
+document.addEventListener('DOMContentLoaded', function(event) {
 
     // Create a function that runs whenever the submit button is clicked
+    document.querySelector('#submit-btn').addEventListener('click', function(e){
 
         //prevent the submit button from refreshing the page
-
-
+        e.preventDefault();
+    
         //Create a variable called moodvalue and get the value of the #mood input
-
-
+        let moodEntry = document.querySelector("#mood").value;
+        
         //Correct for capitalization
+        moodEntry = moodEntry.toLowerCase().trim();
 
         // if the user inputs excited / ecstatic / fantastic change the CSS class to 'excited'
-
+        if(["excited", "ecstatic", "fantastic"].includes(moodEntry)) {
+            document.querySelector(".moodring > div").setAttribute("class", "excited");
+            console.log("excited");
+        } 
         // if the user inputs happy/good/great change the CSS class to 'happy'
-
+        else if(["happy", "good", "great"].includes(moodEntry)) {
+            document.querySelector(".moodring > div").setAttribute("class", "happy");
+            console.log("happy");
+        }
         // if the user inputs bad/angry change the CSS class to 'bad'
-
-    // Listen for user interaction on the submit button.
+        else if(["bad", "angry"].includes(moodEntry)) {
+            document.querySelector(".moodring > div").setAttribute("class", "bad");
+            console.log("bad");
+        }
+    });
+});
--- Week 5 - More JS & Responsive Design/Lesson 09/02_rock_paper_scissors/mes_solution/game.js
+++ Week 5 - More JS & Responsive Design/Lesson 09/02_rock_paper_scissors/mes_solution/game.js
@@ -1,25 +1,73 @@
 
 
 
-
 // Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
 function computerChoice() {
+    //define choices
+    possibleChoices = ["rock", "paper", "scissors"];
+    
+    // create random number between 1 and 3
+    myIndex = Math.floor(Math.random() * 3);
 
+    //return computer choice
+    return possibleChoices[myIndex];
 };
 
 
-/* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
-
- */
+/* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.*/
 function compare(me, opponent) {
+    //target the id computerSelection and put what the computer chose in the text
+    document.querySelector("#computerSelection").innerHTML = opponent;
 
+    //target the decision and display who wins
+
+    let result = "";
+
+    if(me == "rock"){
+        if(opponent == "rock"){
+            result = "Tie";
+        } else if(opponent == "paper"){
+            result = "Computer wins";
+        } else{
+            result = "I win!";
+        }
+    } else if(me == "paper") {
+        if(opponent == "rock"){
+            result = "I win!";
+        } else if(opponent == "paper"){
+            result = "Tie";
+        } else{
+            result = "Computer wins";
+        }
+    } else {
+        if(opponent == "rock"){
+            result = "Computer wins";
+        } else if(opponent == "paper"){
+            result = "I win!";
+        } else{
+            result = "Tie";
+        }
+    }
+
+    document.querySelector("#decision").innerHTML = result;
+    document.querySelector("#computerSelection").innerHTML = opponent;
 };
 
 
 
-jQuery( document ).ready(function() {
+document.addEventListener('DOMContentLoaded', function(event) {
+    // When I click on scissors
+    document.querySelector("#scissors").addEventListener('click', function(e) {
+        compare("scissors", computerChoice());
+    });
 
-    // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+    // When I click on paper
+    document.querySelector("#paper").addEventListener('click', function(e) {
+        compare("paper", computerChoice());
+    });
 
-
+    // When I click on rock
+    document.querySelector("#rock").addEventListener('click', function(e) {
+        compare("rock", computerChoice());
+    });
 });
