7233d2053f3065efabed0602423f69df5164d95f
“mscanzillo” <scanzillom@gmail.com>
2023-06-14 20:37:08 -0400

    week 17 in class exercises

--- Week 9 - Objects & APIs/Lesson 17/02_giphy_api/mes_solution/js/script.js
+++ Week 9 - Objects & APIs/Lesson 17/02_giphy_api/mes_solution/js/script.js
@@ -11,20 +11,34 @@ const apiKey = "i9u5FoDahfPMI2LFAipRD3KXJ45afk0f";
 async function giphySearch(e) {
 
   // prevent the page from submitting/reloading
+  e.preventDefault();
 
   // get the user input from the form
+  searchTerm = document.querySelector('input[name="search-term"]').value;
 
   // make an API call using fetch() - include your API key and the user's search term (template strings are your friend)
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}`;
+
+  const giphyResponse = await fetch(apiUrl);
 
   // convert your response data into .json()
+  const giphyJSON = await giphyResponse.json();
 
   // print your data to the console to see its format, dont forget to delete later
+  console.log(giphyJSON);
 
   // clear out all gifs from previous searches
+  document.querySelector('#giphy-results').innerHTML = "";
+
 
   // use a loop to create and append each image to the dom
-
+  for(i=0; i<giphyJSON.data.length; i++){ 
+    let gif = document.createElement("img");
+    gif.setAttribute("src", giphyJSON.data[i].images.fixed_width.url);
+    document.querySelector("#giphy-results").appendChild(gif);
+  }
 }
 
 // dont forget your event listener
+document.querySelector('input[type="submit"]').addEventListener('click', giphySearch);
 
