2c7fe01dfdc68cb19254c8e8492bf1d3a5d17e1b
“mscanzillo” <scanzillom@gmail.com>
2023-06-21 20:51:40 -0400

    committing all the files from the bad intern exercise

---
+++ BirdFeed/js/scripts.js
@@ -0,0 +1,10 @@
+key="9v4e5m02o3t4"
+
+
+// TODO: Is there a loading spinner I can use? How do i use it?
+async function handleSpeciesRequest(e) {
+    // Make API request with relevant region code 
+}
+
+// Add event listener to the submit button, get species searched for when submitted
+// How do i make sure i'm submitting the species code vs. the species name? should i show a preset list of species? Is there a vanilla JS component I can use for that?
